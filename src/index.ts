6474b476e6f238a31644482b93136ada2151b4f1
johannes-z <johannes.zwirchmayr@gmail.com>
2024-05-20 13:38:12 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { kebabize } from './utils/kebabize'
 
 const REGEX_SpecialAbilities = /(?<name>[A-Za-z\s]+?\s?\((Ex|Su|Sp)\))\s(?<description>.*?)(?=\s[A-Za-z\s]+?\s?\((Ex|Su|Sp)\)|$)/gs
 
-
 async function extractStatblocks(filename: string, destination: string) {
   const file = Bun.file(filename)
   const text = await file.text()
@@ -14,30 +13,38 @@ async function extractStatblocks(filename: string, destination: string) {
   obj.forEach((monster: any) => {
     const name = (Array.isArray(monster.Name) ? monster.Name.join(' ') : monster.Name)
       .replace(/'/g, '')
+
+    delete monster.FullText
+    for (const key in monster) {
+      if (!monster[key])
+        delete monster[key]
+    }
+    monster.AbilityScores = monster.AbilityScores
+      ?.split(/,\s+/g)
+      .map((ability: string) => {
+        const value = ability.split(' ')[1]
+        return value === '-' ? '—' : +value
+      })
+    monster.Senses = monster.Senses?.split(/;\s+/g)
+    monster.Melee = monster.Melee?.split(/\s+or\s{2,}/g)
+    monster.Ranged = monster.Ranged?.split(/\s+or\s{2,}/g)
+    monster.Feats = monster.Feats?.split(/,\s+/g)
+    monster.Skills = monster.Skills?.split(/,\s+/g)
+    monster.Languages = monster.Languages?.split(/[,;]\s+/g)
+    monster.Description = monster.Description?.split(/\s{2,}/g)
+
+    if (monster.SpecialAbilities) {
+      const matches = [...monster.SpecialAbilities.matchAll(REGEX_SpecialAbilities)]
+
+      monster.SpecialAbilities = matches.map(match => ({
+        name: match.groups.name.trim(),
+        description: match.groups.description.trim().replace(/\s{2,}/g, ' '), // Remove extra spaces
+      }))
+    }
+
     const filename = kebabize(name)
     const target = Bun.file(`${destination}/${filename}.yaml`)
-
-    const sanitized = Object.assign({}, monster)
-
-    sanitized.AbilityScores = monster.AbilityScores.split(/,\s+/g).map(ability => ability.split(' ')[1])
-    sanitized.Senses = monster.Senses.split(/;\s+/g)
-    sanitized.Melee = monster.Melee.split(/\s+or\s{2,}/g)
-    sanitized.Ranged = monster.Ranged.split(/\s+or\s{2,}/g)
-    sanitized.Feats = monster.Feats.split(/,\s+/g)
-    sanitized.Skills = monster.Skills.split(/,\s+/g)
-    sanitized.Languages = monster.Languages.split(/[,;]\s+/g)
-    sanitized.Description = monster.Description.split(/\s{2,}/g)
-
-    const matches = [...sanitized.SpecialAbilities.matchAll(REGEX_SpecialAbilities)]
-
-    sanitized.SpecialAbilities = matches.map(match => ({
-      name: match.groups.name.trim(),
-      description: match.groups.description.trim().replace(/\s{2,}/g, ' '), // Remove extra spaces
-    }))
-
-    delete sanitized.FullText
-
-    Bun.write(target, dump(sanitized))
+    Bun.write(target, dump(monster))
   })
 }
 
