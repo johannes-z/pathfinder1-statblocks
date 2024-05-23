0956f7776f6423f505580faa9f7ae5edbd53d76b
johannes-z <johannes.zwirchmayr@gmail.com>
2024-05-23 20:20:33 +0200

    cleanup

--- src/utils/unique.ts
+++
@@ -1,8 +0,0 @@
-// function that takes and array of objects and a string parameter that is the key of the object. the function returns the array of objects with unique values for the key parameter.
-export function unique<T>(array: T[], key: keyof T, key2?: keyof T): T[] {
-  const seen = new Set()
-  return array.filter((item) => {
-    const value = key2 ? `${item[key]}${item[key2]}` : item[key]
-    return seen.has(value) ? false : seen.add(value)
-  })
-}
